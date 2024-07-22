60dff379542dd561d163143afb64f4999ae1df49
ipv6wiz <john@thejohnpage.com>
2024-07-21 17:38:05 -0700

    Deployed Version 0.0.338. Added BankInfo - bug fixes - interface to class

--- src/app/theme/shared/dtos/advance-bank.dto.ts
+++ src/app/theme/shared/dtos/advance-bank.dto.ts
@@ -1,6 +1,6 @@
-export interface AdvanceBankDto {
-  bankAccountName: string; // name on account
-  bankName: string; // name of bank
-  bankWireRoutingNumber: string; // bank routing number for wires
-  bankAccountNumber: string; // bank account number
+export class AdvanceBankDto {
+  bankAccountName?: string; // name on account
+  bankName?: string; // name of bank
+  bankWireRoutingNumber?: string; // bank routing number for wires
+  bankAccountNumber?: string; // bank account number
 }
