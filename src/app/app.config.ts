011567a03299becd4958e4f170dc62c6f35b69ec
ipv6wiz <john@thejohnpage.com>
2024-08-08 21:47:43 -0700

    Deployed Version 0.0.368. EOD: August 8th 2024

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,3 @@
 export class AppConfig {
-  version: string = '0.0.367';
+  version: string = '0.0.368';
 }
--- src/app/theme/shared/dtos/ledger-item.dto.ts
+++ src/app/theme/shared/dtos/ledger-item.dto.ts
@@ -6,6 +6,7 @@ export interface LedgerItemDto {
   advanceId: string;
   advanceName: string;
   description: string;
+  advanceAgreementNumber: string;
   amount: number; // $0.00
   balance: number; // updated on the fly
 }
